332d779230a04b5ea6b49f9f264aa3dab5668d72
mirmik <mirmikns@yandex.ru>
2022-05-20 20:00:36 +0300

    'observer'

--- src/Observer.cpp
+++ src/Observer.cpp
@@ -5,3 +5,8 @@ Observer::Observer(QWidget *parent) : QWidget(parent)
     setMinimumSize(200, 200);
     setWindowTitle("VisualZone");
 }
+
+void Observer::add_subscriber(igris::buffer theme) 
+{
+    subscribers.push_back(crow::subscriber_node(crow::crowker_address(), theme));
+}
--- src/Observer.h
+++ src/Observer.h
@@ -1,10 +1,15 @@
 #include <QtWidgets>
+#include <crow/nodes/subscriber_node.h>
 
 class Observer : public QWidget
 {
     Q_OBJECT
 
+    std::vector<crow::subscriber_node> subscribers;
+
     public:
         Observer(QWidget *parent = nullptr);
         ~Observer() = default;
+
+    void add_subscriber(igris::buffer theme);
 };
